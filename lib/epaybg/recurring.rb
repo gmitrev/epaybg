b079d3475ff7ddfc311085330c48e48ee3f46203
Georgi Mitrev <gvmitrev@gmail.com>
2016-03-04 19:19:07 +0200

    Update styling and remove useless comments
    Preparing for 1.0.0

--- lib/epaybg/recurring.rb
+++ lib/epaybg/recurring.rb
@@ -6,9 +6,6 @@ require 'epaybg/recurring/debt/response'
 module Epaybg
   module Recurring
     def self.parse_request_body(body)
-      # The \s could be replaced with \r but for now i will leave it be
-      # It will be changed after we complate some test with EpayBG
-
       array = body.split(/\s/).reject(&:empty?).compact
       array.inject({}) do |hash, element|
         key, value = *element.strip.split('=')
--- lib/epaybg/recurring/debt/response.rb
+++ lib/epaybg/recurring/debt/response.rb
@@ -17,9 +17,11 @@ module Epaybg
 
         def initialize(params = {})
           @errors = []
+
           params.each do |k, v|
             instance_variable_set("@#{k}", v)
           end
+
           yield self if block_given?
           validate!
         end
@@ -31,13 +33,9 @@ module Epaybg
 
           @errors << "'xvalidto' should be a time type field!" unless xvalidto.kind_of?(Time)
 
-          # Validation of the elemnts length
-
-          { secondid: 15, shortdesc: 40, longdesc: 1800 }.each do |k, v|
+          {secondid: 15, shortdesc: 40, longdesc: 1800}.each do |k, v|
             @errors << "Attribute #{k} is too long. Maximum length should be #{v}." if send(k).to_s.length > v
           end
-
-          # Check status format
 
           @errors << "Invalid value #{status} for status" unless STATUSES.include?(status)
         end
@@ -47,8 +45,8 @@ module Epaybg
         end
 
         def longdesc
-          # Escape new lines in long desc
           return nil unless @longdesc
+
           @longdesc.gsub("\n", "\\n")
         end
 
